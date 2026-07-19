b0583b410a708fdcafc6fdf2be1422daa629cc22
Sumiya Kuroda <26479377+sumiya-kuroda@users.noreply.github.com>
2026-07-19 17:13:31 +0100

    Minor fix on home.nix for cayde

--- machines/cayde/home.nix
+++ machines/cayde/home.nix
@@ -67,7 +67,7 @@
     # `initContent`.
     ########################################################################
     initContent = ''
-      # >>> conda initialize >>>
+      export MAMBA_ROOT_PREFIX="$HOME/miniforge3"
       if [ -f "$HOME/miniforge3/etc/profile.d/conda.sh" ]; then
         . "$HOME/miniforge3/etc/profile.d/conda.sh"
       fi
